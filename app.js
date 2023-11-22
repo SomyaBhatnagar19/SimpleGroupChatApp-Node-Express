0b30a8aad98505dacec307bb8f30baf705079139
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-11-22 10:35:20 +0530

    Making a simple login form without the submit functionality.

--- app.js
+++ app.js
@@ -3,8 +3,22 @@
 const express = require("express");
 const app = express();
 
+// const username = localStorage.getElementById('email');
+// console.log(username);
+
 app.use("/", (req, res, next) => {
-  res.send("<h1>Simple Chat App</h1>");
+  res.send(`
+  <html>
+    <form>
+    <h2> Login Page </h2>
+      <label>Email Id: </label>
+      <input type="email" name="email" id="email"/>
+      <button type="submit">
+        Login
+      </button>
+    </form>
+  </html>
+`);
 });
 
 app.listen(3000);
