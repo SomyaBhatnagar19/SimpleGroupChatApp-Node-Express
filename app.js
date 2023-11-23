d8d34ba75e2e0ecbd81380523ae856c8caefcc46
SomyaBhatnagar19 <somya.bhatnagar98@gmail.com>
2023-11-23 10:59:02 +0530

    Adding a page not found error html file and connecting it to app.js.

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const path = require('path');
 const express = require("express");
 /* Initialising a file where the user messages can be stored */
 const userMessageFiles = {};
@@ -17,6 +18,10 @@ app.use((req, res, next) => {
 app.use('/', loginRouter);
 app.use('/chat', chatRouter);
 
+/* Code for 404 error & its conncetion with the views html file */
+app.use((req, res, next) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
 app.listen(3000, () => {
   console.log("Server is running on port 3000");
 });
